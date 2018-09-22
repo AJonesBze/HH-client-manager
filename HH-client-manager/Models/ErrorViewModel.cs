5b26a6c2e3f1e6c4fbdfd2418c4ebebfac128cbc
Matthew D <n00721843@ospreys.unf.edu>
2018-09-21 20:36:36 -0400

    Initial template (Web App MVC, 2.1)

---
+++ HH-client-manager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HH_client_manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
