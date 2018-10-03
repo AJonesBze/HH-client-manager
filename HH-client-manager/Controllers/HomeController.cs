48d4092294787ce6b2f9e3202aa5424565c6b539
ArianneKJones <akjonesbze@gmail.com>
2018-10-02 23:47:09 -0400

    created the M, V, and C related to PCL form

--- HH-client-manager/Controllers/HomeController.cs
+++ HH-client-manager/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace HH_client_manager.Controllers
             return View();
         }
 
+        public IActionResult PCLform()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ HH-client-manager/Models/PCL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HH_client_manager.Models
+{
+    public class PCL
+    {
+        public String PatientName { get; set; }
+        public DateTime DateToday { get; set; }
+        public String Event { get; set; }
+        public DateTime EventDate { get; set; }
+        public String Response1 { get; set; }
+        public String Response2 { get; set; }
+        public String Response3 { get; set; }
+        public String Response4 { get; set; }
+        public String Response5 { get; set; }
+        public String Response6 { get; set; }
+        public String Response7 { get; set; }
+        public String Response8 { get; set; }
+        public String Response9 { get; set; }
+        public String Response10 { get; set; }
+        public String Response11 { get; set; }
+        public String Response12 { get; set; }
+        public String Response13 { get; set; }
+        public String Response14 { get; set; }
+        public String Response15 { get; set; }
+        public String Response16 { get; set; }
+        public String Response17 { get; set; }
+
+
+    }
+}
