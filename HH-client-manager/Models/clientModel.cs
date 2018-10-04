c8b21683be7a033281343314b900ff88edbd4fd1
Joebetbeze <34607252+Joebetbeze@users.noreply.github.com>
2018-10-04 13:25:02 -0400

    Last minute commit

---
+++ HH-client-manager/Models/clientModel.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace HH-client-manager.Models 
+{
+    public class Client {
+        public int ID { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public string Ethnicity { get; set; }
+        public int Age { get; set; }
+        public string PartnerGender { get; set; }
+        public string RelationshipStatus { get; set; }
+        public DateTime DateEnrolled { get; set; }
+	public string Counselor Name { get; set; }
+        public int GroupsAttended { get; set; }
+        public int CouncelingSessionsAttended { get; set; }
+        public int MeetingsWithAdvocates { get; set; }
+        public DateTime ExitDate { get; set; }
+        public int GroupsAttended { get; set; }
+        public int HasHarkAssessmentPRE { get; set; }
+        public int HasHarkAssessmentPOST { get; set; }
+        public int HasPCLAssessmentPRE { get; set; }
+        public int HasPCLAssessmentPOST { get; set; }
+        public int HasShelterAssessmentPRE { get; set; }
+        public int HasShelterAssessmentPOST { get; set; }
+    }
+}
