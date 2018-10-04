3809b80e01d19ac5f42ba26d9bbdc15198fc0c28
Matthew D <n00721843@ospreys.unf.edu>
2018-10-04 03:08:41 -0400

    Exporting: Proof of concept

---
+++ HH-client-manager/Models/TestingClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HH_client_manager.Models
+{
+    public class TestingClass // a sample, dummy class used for testing the Excel import/export functionality
+{
+        [DisplayName("Name")]
+        public string name { get; set; }
+        [DisplayName("Statistic")]
+        public int stat { get; set; }
+
+        public string altName { get; set; }
+
+        public TestingClass(string pname, int pstat, string paltname)
+        {
+            name = pname;
+            stat = pstat;
+            altName = paltname;
+        }
+
+    }
+
+
+}
