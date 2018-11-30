8e491b5855e447f0c4250feec852ab8e11cba2eb
ArianneKJones <akjonesbze@gmail.com>
2018-11-30 03:54:21 -0500

    PCL Assessment view and model link

---
+++ HH-client-manager/Controllers/PCLAssessmentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HH_client_manager.Controllers
+{
+    public class PCLAssessmentController : Controller
+{
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        return View();
+    }//end of Index
+}//end of PCLAssessmentController
+}//end of namespace
